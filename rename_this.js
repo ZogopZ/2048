7ddd67c50403a597cb8fcdb66a0f7c9552a0f8e6
ZogopZ <zoiszogop@gmail.com>
2020-11-03 23:06:40 +0200

    2048-development_v3 -All modifications below took place in parent directory.
    -Reworked squares in files 'rename_this.html', 'rename_this.css',
     'rename_this.js' to use 'div' instead of 'canvas' for now.
    -Excluded file 'delete_this.js' from version control.

--- rename_this.js
+++ rename_this.js
@@ -1,50 +1,35 @@
-document.addEventListener("DOMContentLoaded", canvasDrawer);
-document.addEventListener("DOMContentLoaded", function(){
-    document.onkeydown = checkKey;
-    function checkKey(e) {
-        e = e || window.Event;
-        if (e.key == 'ArrowUp') {
-            alert('up was pressed');
-        }
-        else if (e.key == 'ArrowDown') {
-            alert('down was pressed');
-        }
-        else if (e.key == 'ArrowLeft') {
-            alert('left was pressed');
-        }
-        else if (e.key == 'ArrowRight') {
-            alert('right was pressed');
-        }
+'use strict';
+document.addEventListener("DOMContentLoaded", () => {
+    const gameGrid = document.querySelector('.game-grid');
+    let squaresArray = [];
 
+    function createSquares() {
+        for (let x = 0; x < 16; x ++) {
+            let gameSquare = document.createElement('div');
+            gameGrid.appendChild(gameSquare);
+            squaresArray.push(gameSquare);
+        }
     }
+    createSquares();
 });
 
-function canvasDrawer() {
-    let canvas = document.getElementById("myCanvas");
-    let ctx = canvas.getContext("2d");
-    for (let x = 20; x <= 335; x += 105) {
-        for (let y = 20; y <= 335; y += 105) {
-            console.log(x, y);  // todo: delete this comment.
-            ctx.fillStyle = "#D3D3D3D3";
-            ctx.beginPath();
-            ctx.rect(x, y, 100, 100);  // Use static sizes to create squares for the game.
-            ctx.closePath();
-            ctx.fill();
-            ctx.beginPath();
-            ctx.fillStyle = "#FFFFF0";
-            ctx.rect(x+2, y+2, 96, 96);
-            ctx.closePath();
-            ctx.fill();
-            ctx.fillStyle = "#000000";
-            ctx.beginPath();
-            ctx.fillText('number', x+40, y+50, 100);
-            ctx.fill();
-        }
-    }
-    ctx.beginPath();
-    ctx.fillStyle = "#FFFFF0"
-    ctx.rect(600, 200, 100, 100);
-    ctx.fill();
-    ctx.closePath();
-}
-
+// document.addEventListener("DOMContentLoaded", arrowKeyCapture);
+// function arrowKeyCapture() {
+//     document.onkeydown = checkKey;
+//     function checkKey(e) {
+//         e = e || window.Event;
+//         if (e.key == 'ArrowUp') {
+//             alert('up was pressed');
+//         }
+//         else if (e.key == 'ArrowDown') {
+//             alert('down was pressed');
+//         }
+//         else if (e.key == 'ArrowLeft') {
+//             alert('left was pressed');
+//         }
+//         else if (e.key == 'ArrowRight') {
+//             alert('right was pressed');
+//         }
+//
+//     }
+// }
