cf7a714014093e25c4fe5c5165e9c46c87195131
ZogopZ <zoiszogop@gmail.com>
2020-11-05 02:30:12 +0200

    2048-development_v6 -All modifications below took place in parent directory.
    -Reworked game's logic on how to handle zeros and other numbers when
     pressing the up arrow key. A map is used to collect the values of the
     game's tiles, rearrange them according the game's rules and then
     remap the generated values on the correct game tiles.

--- 2048.js
+++ 2048.js
@@ -23,7 +23,6 @@ document.addEventListener("DOMContentLoaded", () => {
             let randomIndex = Math.floor(Math.random() * 16);
             if (squaresArray[randomIndex].innerHTML === '0') {
                 squaresArray[randomIndex].innerHTML = 2;
-                // squaresArray[randomIndex].style.backgroundColor = 'lightgray';  // todo: create a function to check colors for all colors.
             }
             else if (squaresArray[randomIndex].innerHTML !== '0') {
                 generateRandom();
@@ -31,8 +30,9 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
     function generateNoRandom() {
-        squaresArray[0].innerHTML = 4;
-        squaresArray[4].innerHTML = 4;
+        // squaresArray[0].innerHTML = 0;
+        squaresArray[4].innerHTML = 2;
+        // squaresArray[8].innerHTML = 2;
     }
 
     function updateColors()  // todo: probably make this somehow faster?
@@ -71,20 +71,68 @@ document.addEventListener("DOMContentLoaded", () => {
         function checkKey(e) {
             e = e || window.Event;
             if (e.key === 'ArrowUp') {
-                for (let i = 0; i < 11; i++) {
-                    // console.log(squaresArray[i].innerHTML);  // todo: delete this.
-                    if (squaresArray[i].innerHTML === '0' && squaresArray[i+4].innerHTML !== '0') {
-                        squaresArray[i].innerHTML = squaresArray[i+4].innerHTML;
-                        squaresArray[i+4].innerHTML = 0;
-                    }
-                    else if (squaresArray[i].innerHTML === squaresArray[i+4].innerHTML) {
-                        squaresArray[i].innerHTML <<= 1;  // todo: why is this behaving correctly?
-                        squaresArray[i+4].innerHTML = 0;
-                    }
-                //         // squaresArray[i+4].innerHTML = '0';
-                //     }
+                let firstCol = [];
+                let secondCol = [];
+                let thirdCol = [];
+                let fourthCol = [];
+                let masterArray = [];
+                for (let i = 0; i < 16; i += 4) {
+                    if (squaresArray[i].innerHTML !== '0')  // Remove zeroes. todo: more on this.
+                        firstCol.push(+squaresArray[i].innerHTML);
+                    if (squaresArray[i + 1].innerHTML !== '0')
+                        secondCol.push(+squaresArray[i + 1].innerHTML);
+                    if (squaresArray[i + 2].innerHTML !== '0')
+                        thirdCol.push(+squaresArray[i + 2].innerHTML);
+                    if (squaresArray[i + 3].innerHTML !== '0')
+                        fourthCol.push(+squaresArray[i + 3].innerHTML);
+                }
+                masterArray.push(firstCol, secondCol, thirdCol, fourthCol);
+                rearrangeMaster();
+                reMap();
+
+                function rearrangeMaster() {
+                    masterArray.forEach(function(column) {
+                        let iterator = 4 - column.length;  // todo: find a solution for this.
+                        if (column.length === 2) {
+                            if (column[0] === column[1]) {
+                                column[0] <<= 1;
+                                column[1] = 0;
+                            }
+                        }
+                        else if (column.length === 3) {
+                            if (column[0] === column[1] === column[2]) {
+                                column[0] <<= 1;
+                                column[2] = 0;
+                            }
+                        }
+                        for (let i = 0; i < iterator; i++)
+                            column.push(0);
+                    });
+                }
+
+                function reMap() {  // todo: this is done in a different way for each direction key pressed.
+                    masterArray.forEach(function(map, index) {
+                        for (let i = 0; i < 4; i++) {
+                            // console.log(4*i+index);
+                            squaresArray[4 * i + index].innerHTML = map[i];
+                            }
+                    });
                 }
             }
+                    // console.log(squaresArray[i].innerHTML);  // todo: delete this.
+                    // if (squaresArray[i].innerHTML === '0' && squaresArray[i+4].innerHTML !== '0') {
+                    //     continue;
+                        // squaresArray[i].innerHTML = squaresArray[i+4].innerHTML;
+                        // squaresArray[i+4].innerHTML = 0;
+                    // }
+                    // else if (squaresArray[i].innerHTML === squaresArray[i+4].innerHTML) {
+                    //     squaresArray[i].innerHTML <<= 1;  // todo: why is this behaving correctly?
+                    //     squaresArray[i+4].innerHTML = 0;
+                    // }
+                //         // squaresArray[i+4].innerHTML = '0';
+                //     }
+                // }
+            // }
             else if (e.key === 'ArrowDown') {
                 alert('down was pressed');
             }
@@ -94,8 +142,8 @@ document.addEventListener("DOMContentLoaded", () => {
             else if (e.key === 'ArrowRight') {
                 alert('right was pressed');
             }
-            updateColors();
             generateRandom();
+            updateColors();
         }
     }
 });
